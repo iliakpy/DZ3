feca1cb376757c5bdb58a4882856cfc69828df25
iliakpy <56565312+iliakpy@users.noreply.github.com>
2019-10-30 17:19:12 +0300

    Create 2seq.py

---
+++ 2seq.py
@@ -0,0 +1,5 @@
+elements = input('Введи элементы списка через запятую: ')
+new_list = list(int(i) for i in elements.replace(';', ',').replace('/', ',').split(','))
+print(new_list)
+new_set = set(new_list)
+print(new_set)
