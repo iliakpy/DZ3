a903b39a16ad06bb350db9b919bbb34d5c7d044c
iliakpy <56565312+iliakpy@users.noreply.github.com>
2019-10-30 17:20:09 +0300

    Create 3seq.py

---
+++ 3seq.py
@@ -0,0 +1,8 @@
+elements_1 = input('Введи элементы списка №1: ')
+list_1 = list(int(i) for i in elements_1.replace(';', ',').replace('/', ',').split(','))
+set_1 = set(list_1)
+elements_2 = input('Введи элементы списка №2: ')
+list_2 = list(int(i) for i in elements_2.replace(';', ',').replace('/', ',').split(','))
+set_2 = set(list_2)
+result = set(list_1) - set(list_2)
+print(result)
